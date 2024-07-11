26c89c10c454638f6c1c81cbae3bf4d824335185
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-11 22:27:27 +0530

    StarPattern

--- src/ThirdPattern.java
+++ src/ThirdPattern.java
@@ -1,11 +1,13 @@
 import java.util.*;
-public class Starpattern {
+public class ThirdPattern {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-       // int n=sc.nextInt();
+        //int n=5;
         for(int i=1; i<=7; i++){
             for(int j=1; j<=7; j++){
-                if(i==3||i==5||((i==1||i==7)&&j==4)||(i==4&&(j==2||j==6))||((i==2||i==6)&&(j==3||j==5))){
+                if(i==4&&(j==6||j==7)){
+                    System.out.print(" ");
+                }
+                else if(i==1||i==4||i==7||j==1){
                     System.out.print("*");
                 }
                 else{
@@ -15,5 +17,4 @@ public class Starpattern {
             System.out.println();
         }
     }
-
 }
